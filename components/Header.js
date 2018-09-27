dbf7e9e1a47a2b61aa2e98a5380dd9f2e1dc6da8
Yingjian Li <yingjli@thoughtworks.com>
2018-09-27 22:15:20 +0800

    Use jsx style in header

--- components/Header.js
+++ components/Header.js
@@ -1,38 +1,43 @@
 import Navigation from "./Navigation";
+import Link from "next/link";
 
 export default () => {
   return (
-    <header
-      style={{
-        boxShadow: "0 0 1px rgba(0,0,0,0.2)",
-        flex: "0 0 auto"
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          margin: "0 auto",
-          paddingTop: 20,
-          maxWidth: 1000,
-          paddingBottom: 20
-        }}
-      >
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div
-            style={{
-              display: "inline-block",
-              fontWeight: "bold",
-              fontSize: 24,
-              fontFamily: `"Oswald", sans-serif`
-            }}
-          >
-            Star Boy
-          </div>
+    <header>
+      <div className="header-container">
+        <div className="slogan">
+          <Link href="/">
+            <a>Star Boy</a>
+          </Link>
         </div>
         <Navigation />
       </div>
+      <style jsx>{`
+        header {
+          box-shadow: 0 0 1px rgba(0, 0, 0, 0.2);
+          flex: 0 0 auto;
+        }
+        .header-container {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin: 0 auto;
+          padding-top: 20px;
+          max-width: 1000px;
+          padding-bottom: 20px;
+        }
+        div.slogan {
+          display: inline-block;
+          font-weight: bold;
+          font-size: 24px;
+          font-family: "Oswald", sans-serif;
+        }
+        a {
+          text-decoration: none;
+          cursor: pointer;
+          color: rgba(0, 0, 0, 0.7);
+        }
+      `}</style>
     </header>
   );
 };
