95889bb20e9d80c71083f6c41f0ec52f28a86e94
Yingjian Li <qxlee65@126.com>
2018-11-13 21:43:34 +0800

    Add util function to help parse meta info at the top of post

--- utils/markdown.js
+++ utils/markdown.js
@@ -2,11 +2,25 @@ export const getTitle = content => /#(.*)/g.exec(content)[1];
 
 export const sanitizePost = post => /\-{3}\n(.*)/s.exec(post)[1];
 
+export const getMetaInfo = post => {
+  const metaContent = /(.*)\-{3}\n/s.exec(post)[1];
+  const [keyRow, _, valueRow] = metaContent.split("\n");
+
+  const keys = keyRow.replace(/\s/g, "").split("|");
+  const values = valueRow.replace(/\s/g, "").split("|");
+
+  return keys.reduce(
+    (acc, cur, index) =>
+      Object.assign(acc, cur ? { [cur]: values[index] } : {}),
+    {}
+  );
+};
+
 export const getPostSummary = content => {
   return content
     .slice(0, 900)
     .replace(/#(.*)/g, "")
-    .replace(/!?\[(.*?)\]\((.*?)\)/g, (match, text) => text)
+    .replace(/!?\[(.*?)\]\((.*?)\)/g, (_, text) => text)
     .replace(/[\n#_>\[\]]/g, "")
     .replace(/\(http.*/, "")
     .trim();
